bfb6808616359262025711197fb6788f709768e3
Vitaly Sizov <vitalii_sizov@mail.ru>
2017-05-30 22:00:02 +0600

    Solution to 6 chapter

--- app/src/main/java/com/sizov/vitaly/geoquiz/CheatActivity.java
+++ app/src/main/java/com/sizov/vitaly/geoquiz/CheatActivity.java
@@ -1,11 +1,15 @@
 package com.sizov.vitaly.geoquiz;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -18,6 +22,7 @@ public class CheatActivity extends AppCompatActivity {
     private boolean mAnswerIsTrue;
     private boolean mShowAnswerPressed;
     private TextView mAnswerTextView;
+    private TextView mShowApiLevel;
     private Button mShowAnswer;
 
     public static Intent newIntent(Context packageContext, boolean answerIsTrue) {
@@ -48,6 +53,10 @@ public class CheatActivity extends AppCompatActivity {
         mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
         mAnswerTextView = (TextView) findViewById(R.id.answer_text_view);
 
+        // Вывод версии API
+        mShowApiLevel = (TextView) findViewById(R.id.show_api_level);
+        mShowApiLevel.setText("API level " + String.valueOf(Build.VERSION.SDK_INT));
+
         if (savedInstanceState != null) {
             mShowAnswerPressed = savedInstanceState.getBoolean(KEY_CHEATED);
             setAnswerShownResult(mShowAnswerPressed);
@@ -64,6 +73,27 @@ public class CheatActivity extends AppCompatActivity {
                 }
                 mShowAnswerPressed = true;
                 setAnswerShownResult(true);
+
+                // Анимаация
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    int cx = mShowAnswer.getWidth() / 2 ;
+                    int cy = mShowAnswer.getHeight() / 2;
+                    float radius = mShowAnswer.getWidth();
+                    Animator anim = ViewAnimationUtils
+                            .createCircularReveal(mShowAnswer, cx, cy, radius, 0);
+                    anim.addListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        super.onAnimationEnd(animation);
+                        mAnswerTextView.setVisibility(View.VISIBLE);
+                        mShowAnswer.setVisibility(View.VISIBLE);
+                    }
+                    });
+                    anim.start();
+                } else {
+                mAnswerTextView.setVisibility(View.VISIBLE);
+                mShowAnswer.setVisibility(View.VISIBLE);
+                }
             }
         });
     }
